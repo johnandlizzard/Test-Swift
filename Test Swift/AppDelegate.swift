8ee561a4df7e3fa9687b3174f0607cee07213ad8
Liz Cooner <ecooner33@gmail.com>
2017-05-02 14:13:02 -0400

    Initial Commit

---
+++ Test Swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Test Swift
+//
+//  Created by Elizabeth on 5/2/17.
+//  Copyright © 2017 Lizzard Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Test Swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Test Swift
+//
+//  Created by Elizabeth on 5/2/17.
+//  Copyright © 2017 Lizzard Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
